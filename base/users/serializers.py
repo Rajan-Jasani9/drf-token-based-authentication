05841d7ae61038f75fe8fe09c50aa1c07d3acdd0
Rajan-Jasani9 <rajan.jasani2002@gmail.com>
2024-06-24 23:01:38 +0530

    feat: initial commit

---
+++ base/users/serializers.py
@@ -0,0 +1,75 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.exceptions import ValidationError
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+        
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    data['user'] = user
+                else:
+                    raise serializers.ValidationError("User account is disabled.")
+            else:
+                raise serializers.ValidationError("Invalid credentials.")
+        else:
+            raise serializers.ValidationError("Must include 'username' and 'password'.")
+        
+        return data
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
+    username = serializers.CharField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "first_name",
+                  "last_name", "email", "password", "password2"]
+        extra_kwargs = {
+            'password': {"write_only": True}
+        }
+
+    def validate_username(self, username):
+        if User.objects.filter(username=username).exists():
+            detail = {
+                "detail": "User Already exist!"
+            }
+            raise ValidationError(detail=detail)
+        return username
+
+    def validate(self, instance):
+        if instance['password'] != instance['password2']:
+            raise ValidationError({"message": "Both password must match"})
+
+        if User.objects.filter(email=instance['email']).exists():
+            raise ValidationError({"message": "Email already taken!"})
+
+        return instance
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        password2 = validated_data.pop('password2')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
